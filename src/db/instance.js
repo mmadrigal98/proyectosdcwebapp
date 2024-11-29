983911bd3c8cd1f14498414cc478b8c35df0c60c
mmadrigal98 <63423756+mmadrigal98@users.noreply.github.com>
2024-11-29 17:54:25 -0600

    Update instance.js

--- src/db/instance.js
+++ src/db/instance.js
@@ -1,8 +1,3 @@
 import { Sequelize } from 'sequelize'
-import betterSqlite3 from 'better-sqlite3'
 
-export const db = new Sequelize({
-  dialect: 'sqlite',
-  storage: ':memory:', // Use in-memory SQLite database
-  dialectModule: betterSqlite3, // Use better-sqlite3 as the SQLite driver
-})
+export const db = new Sequelize('sqlite::memory:')
